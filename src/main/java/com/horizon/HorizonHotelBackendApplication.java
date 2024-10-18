10745ddb590dc49c4f79c3464f0536a337429909
dinhhung1084 <dinhhung41335@gmail.com>
2024-10-18 10:00:13 +0700

    Initial commit

---
+++ src/main/java/com/horizon/HorizonHotelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.horizon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorizonHotelBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorizonHotelBackendApplication.class, args);
+    }
+
+}
