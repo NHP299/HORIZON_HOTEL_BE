78c661ad09f60e5b42f8a5b015c195a3d96f21af
dinhhung1084 <dinhhung41335@gmail.com>
2024-10-19 20:45:02 +0700

    delete isActivated

--- src/main/java/com/horizon/dto/ServicesDto.java
+++ src/main/java/com/horizon/dto/ServicesDto.java
@@ -6,7 +6,6 @@ public class ServicesDto {
     private Integer id;
     private Integer roomTypeId;
     private String description;
-    private Boolean isActivated;
     private Timestamp startedTime;
     private Timestamp endTime;
 }
