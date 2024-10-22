2a5f91f271f8fc27b93350cbd0dcdbcea3da07a1
unknown <dinhtcpd08605@fpt.edu.vn>
2024-10-22 16:24:33 +0700

    add mapper

--- src/main/java/com/horizon/mapper/RoomTypeMapper.java
+++ src/main/java/com/horizon/mapper/RoomTypeMapper.java
@@ -1,4 +1,19 @@
 package com.horizon.mapper;
 
+import com.horizon.domain.RoomType;
+import com.horizon.dto.RoomTypeDto;
+
 public class RoomTypeMapper {
+
+    public static RoomTypeDto mapToRoomTypeDto(RoomType roomType) {
+        return new RoomTypeDto(roomType.getId(),
+                roomType.getName(),
+                roomType.getDescription());
+    }
+
+    public static RoomType mapToRoomType(RoomTypeDto roomTypeDto) {
+        return new RoomType(roomTypeDto.getId(),
+                roomTypeDto.getName(),
+                roomTypeDto.getDescription());
+    }
 }
