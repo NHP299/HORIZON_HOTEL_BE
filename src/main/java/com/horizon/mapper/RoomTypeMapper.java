c845e2d5ed6c5773f84046305bb46f2cbf8450ff
unknown <dinhtcpd08605@fpt.edu.vn>
2024-10-24 10:53:20 +0700

    change to interface

--- src/main/java/com/horizon/mapper/RoomTypeMapper.java
+++ src/main/java/com/horizon/mapper/RoomTypeMapper.java
@@ -3,17 +3,7 @@ package com.horizon.mapper;
 import com.horizon.domain.RoomType;
 import com.horizon.dto.RoomTypeDto;
 
-public class RoomTypeMapper {
-
-    public static RoomTypeDto mapToRoomTypeDto(RoomType roomType) {
-        return new RoomTypeDto(roomType.getId(),
-                roomType.getName(),
-                roomType.getDescription());
-    }
-
-    public static RoomType mapToRoomType(RoomTypeDto roomTypeDto) {
-        return new RoomType(roomTypeDto.getId(),
-                roomTypeDto.getName(),
-                roomTypeDto.getDescription());
-    }
+public interface RoomTypeMapper {
+    RoomTypeDto mapToRoomTypeDto(RoomType roomType);
+    RoomType mapToRoomType(RoomTypeDto roomTypeDto);
 }
