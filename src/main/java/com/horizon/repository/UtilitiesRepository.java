8fe75a646dff23affb6a6037ffbbad8e2ea57d08
Hoai2004 <levanhoai2004@gmail.com>
2024-10-25 17:25:52 +0700

    add search and pagination functionality

--- src/main/java/com/horizon/repository/UtilitiesRepository.java
+++ src/main/java/com/horizon/repository/UtilitiesRepository.java
@@ -1,9 +1,17 @@
 package com.horizon.repository;
 
 import com.horizon.domain.Utilities;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UtilitiesRepository extends JpaRepository<Utilities, Integer> {
+    Page<Utilities> findByNameContainingIgnoreCase(String name, Pageable pageable);
+    Page<Utilities> findByRoomType_NameContainingIgnoreCase(String roomTypeName, Pageable pageable);
+    @Query("SELECT u FROM Utilities u JOIN Room r ON u.roomType.id = r.roomType.id WHERE r.name = :roomName")
+    Page<Utilities> findUtilitiesByRoomName(@Param("roomName") String roomName, Pageable pageable);
 }
