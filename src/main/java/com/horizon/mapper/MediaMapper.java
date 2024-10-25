f9be340fef1e37dc5d938618d718aa4cad11e3d5
thongnqt188 <thongnqt188@gmail.com>
2024-10-25 12:03:56 +0700

    update MediaMapper

--- src/main/java/com/horizon/mapper/MediaMapper.java
+++ src/main/java/com/horizon/mapper/MediaMapper.java
@@ -2,21 +2,10 @@ package com.horizon.mapper;
 
 import com.horizon.domain.Media;
 import com.horizon.dto.MediaDto;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
-@Mapper
 public interface MediaMapper {
-    MediaMapper INSTANCE = Mappers.getMapper(MediaMapper.class);
-
-    @Mapping(source = "roomType.id", target = "roomTypeId")
     MediaDto mediaToMediaDto(Media media);
-
-//    @Mapping(source = "roomTypeId", target = "roomType.id")
-//    Media mediaDtoToMedia(MediaDto mediaDto);
-
     List<MediaDto> mediaListToMediaDtoList(List<Media> mediaList);
 }
--- src/main/java/com/horizon/mapper/impl/MediaMapperImpl.java
+++ src/main/java/com/horizon/mapper/impl/MediaMapperImpl.java
@@ -1,2 +1,37 @@
-package com.horizon.mapper.impl;public class MediaMapperImpl {
+package com.horizon.mapper.impl;
+
+import com.horizon.domain.Media;
+import com.horizon.dto.MediaDto;
+import com.horizon.mapper.MediaMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class MediaMapperImpl implements MediaMapper {
+
+    @Override
+    public MediaDto mediaToMediaDto(Media media) {
+        if (media == null) {
+            return null;
+        }
+
+        MediaDto mediaDto = new MediaDto();
+        mediaDto.setId(media.getId());
+        mediaDto.setRoomTypeId(media.getRoomType().getId());
+        mediaDto.setPath(media.getPath());
+        return mediaDto;
+    }
+
+    @Override
+    public List<MediaDto> mediaListToMediaDtoList(List<Media> mediaList) {
+        if (mediaList == null) {
+            return null;
+        }
+
+        return mediaList.stream()
+                .map(this::mediaToMediaDto)
+                .collect(Collectors.toList());
+    }
 }
