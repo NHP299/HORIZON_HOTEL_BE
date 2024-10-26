377cc07d4c229ba373f0bb5a376d2b80ceee2105
thongnqt188 <thongnqt188@gmail.com>
2024-10-26 19:48:10 +0700

    delete method findByName

--- src/main/java/com/horizon/repository/BannerTypeRepository.java
+++ src/main/java/com/horizon/repository/BannerTypeRepository.java
@@ -4,9 +4,7 @@ import com.horizon.domain.BannerType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface BannerTypeRepository extends JpaRepository<BannerType, Integer> {
-    Optional<BannerType> findByName(String name);  // Thêm phương thức này
+
 }
