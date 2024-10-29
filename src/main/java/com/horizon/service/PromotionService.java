77bc26e44ea482dd748c51db92f1397bd9b153ed
Hoai2004 <levanhoai2004@gmail.com>
2024-10-29 17:44:08 +0700

    update crud method

--- src/main/java/com/horizon/service/PromotionService.java
+++ src/main/java/com/horizon/service/PromotionService.java
@@ -1,5 +1,15 @@
 package com.horizon.service;
 
 
+import com.horizon.dto.PromotionDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface PromotionService {
+    PromotionDto createPromotion(PromotionDto promotionDto);
+    PromotionDto updatePromotion(Integer promotionId,PromotionDto promotionDto);
+    void deletePromotion(Integer promotionId);
+    PromotionDto getPromotionById(Integer promotionId);
+    Page<PromotionDto> getAllPromotions(Pageable pageable);
+
 }
--- src/main/java/com/horizon/service/impl/PromotionServiceImpl.java
+++ src/main/java/com/horizon/service/impl/PromotionServiceImpl.java
@@ -1,20 +1,54 @@
 package com.horizon.service.impl;
 
+import com.horizon.domain.Promotion;
+import com.horizon.dto.PromotionDto;
+import com.horizon.exception.ResourceNotFoundException;
+import com.horizon.mapper.PromotionMapper;
 import com.horizon.repository.PromotionRepository;
 import com.horizon.service.PromotionService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 
 @Service
+@AllArgsConstructor
 public class PromotionServiceImpl implements PromotionService
 {
-    private final PromotionRepository promotionRepository;
+    private PromotionRepository promotionRepository;
+    private PromotionMapper promotionMapper;
 
-    @Autowired
-    private PromotionServiceImpl(PromotionRepository promotionRepository) {
-        this.promotionRepository = promotionRepository;
+    @Override
+    public PromotionDto createPromotion(PromotionDto promotionDto) {
+        Promotion promotion = promotionMapper.maptoPromotion(promotionDto,null);
+        Promotion savedPromotion = promotionRepository.save(promotion);
+        return promotionMapper.maptoPromotionDto(savedPromotion);
     }
 
+    @Override
+    public PromotionDto updatePromotion(Integer promotionId, PromotionDto promotionDto) {
+        Promotion existingPromotion = promotionRepository.findById(promotionId).orElseThrow(() -> new ResourceNotFoundException("Promotion not found" + promotionId));
+        Promotion updatedPromotion = promotionMapper.maptoPromotion(promotionDto,existingPromotion);
+        updatedPromotion = promotionRepository.save(updatedPromotion);
+        return promotionMapper.maptoPromotionDto(updatedPromotion);
+    }
 
+    @Override
+    public void deletePromotion(Integer promotionId) {
+        Promotion promotion = promotionRepository.findById(promotionId).orElseThrow(() -> new ResourceNotFoundException("Promotion not found" + promotionId));
+        promotionRepository.delete(promotion);
+    }
+
+    @Override
+    public PromotionDto getPromotionById(Integer promotionId) {
+        Promotion promotion = promotionRepository.findById(promotionId).orElseThrow(() -> new ResourceNotFoundException("Promotion not found" + promotionId));
+        return promotionMapper.maptoPromotionDto(promotion);
+    }
+
+    @Override
+    public Page<PromotionDto> getAllPromotions(Pageable pageable) {
+        Page<Promotion> promotions = promotionRepository.findAll(pageable);
+        return promotions.map(promotionMapper::maptoPromotionDto);
+    }
 }
