d2182ed2b61a5e16c8023cfd8fd1c2aab86ba55b
Hoai2004 <levanhoai2004@gmail.com>
2024-10-29 17:42:54 +0700

    change data type from Timestamp to LocalDateTime

--- src/main/java/com/horizon/dto/PromotionDto.java
+++ src/main/java/com/horizon/dto/PromotionDto.java
@@ -1,14 +1,22 @@
 package com.horizon.dto;
 
-import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PromotionDto {
     private Integer id;
     private String name;
     private String description;
-    private Integer discountCondition;
-    private Timestamp startTime;
-    private Timestamp endTime;
+    private String discountCondition;
+    private Double discount;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
     private Integer maxUsage;
     private String voucherType;
     private Integer maxAmount;
