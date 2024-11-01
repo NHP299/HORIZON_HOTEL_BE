cdf8b3e4d3412d03c6b277be3561896941cf7593
thongnqt188 <thongnqt188@gmail.com>
2024-11-01 19:23:19 +0700

    banner mapper

--- src/main/java/com/horizon/mapper/BannerMapper.java
+++ src/main/java/com/horizon/mapper/BannerMapper.java
@@ -1,2 +1,12 @@
-package com.horizon.mapper;public class BannerMapper {
+package com.horizon.mapper;
+
+import com.horizon.domain.Banner;
+import com.horizon.dto.BannerDto;
+
+import java.util.List;
+
+public interface BannerMapper {
+    BannerDto bannerToBannerDto(Banner banner);
+    Banner bannerDtoToBanner(BannerDto bannerDto);
+    List<BannerDto> bannerListToBannerDtoList(List<Banner> banners);
 }
