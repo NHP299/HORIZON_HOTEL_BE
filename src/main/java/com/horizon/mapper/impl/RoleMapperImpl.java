1528363215f6d3f3d5746c64bae6203b9b7d0ec9
unknown <dinhtcpd08605@fpt.edu.vn>
2024-11-02 12:43:59 +0700

    add role mapper impl

--- src/main/java/com/horizon/mapper/impl/RoleMapperImpl.java
+++ src/main/java/com/horizon/mapper/impl/RoleMapperImpl.java
@@ -1,4 +1,19 @@
 package com.horizon.mapper.impl;
 
-public class RoleMapperImpl {
+import com.horizon.domain.Role;
+import com.horizon.dto.RoleDto;
+import com.horizon.mapper.RoleMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleMapperImpl implements RoleMapper {
+    @Override
+    public RoleDto toRoleDto(Role role) {
+        return new RoleDto(role.getId(), role.getRoleName());
+    }
+
+    @Override
+    public Role toRole(RoleDto roleDto) {
+        return new Role(roleDto.getId(), roleDto.getRoleName());
+    }
 }
