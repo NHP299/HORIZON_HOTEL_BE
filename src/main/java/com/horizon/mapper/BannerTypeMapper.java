f918559ec78a6297e92c0aa0d02d92c2c1239c28
thongnqt188 <thongnqt188@gmail.com>
2024-11-02 13:51:32 +0700

    detele banner.upload.dir

--- src/main/java/com/horizon/mapper/BannerTypeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.horizon.mapper;
-
-import com.horizon.domain.BannerType;
-import com.horizon.dto.BannerTypeDto;
-
-public interface BannerTypeMapper {
-    BannerTypeDto toDto(BannerType bannerType);
-    BannerType toEntity(BannerTypeDto bannerTypeDto);
-}
