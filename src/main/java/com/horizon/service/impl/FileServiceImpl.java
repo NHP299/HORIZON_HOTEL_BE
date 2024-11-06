30f5fa6130f5076e5b589368ce0748571f446b30
thongnqt188 <thongnqt188@gmail.com>
2024-11-06 14:44:38 +0700

    save and delete file

--- src/main/java/com/horizon/service/impl/FileServiceImpl.java
+++ src/main/java/com/horizon/service/impl/FileServiceImpl.java
@@ -1,2 +1,29 @@
-package com.horizon.service.impl;public class FileServiceImpl {
+package com.horizon.service.impl;
+
+import com.horizon.service.FileService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class FileServiceImpl implements FileService {
+
+    @Override
+    public String saveFile(MultipartFile file, String uploadDir) throws Exception {
+        String filename = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+        Path filePath = Paths.get(uploadDir, filename);
+        Files.copy(file.getInputStream(), filePath);
+        return filename;
+    }
+
+    @Override
+    public void deleteFile(String filename, String uploadDir) throws Exception {
+        Path filePath = Paths.get(uploadDir, filename);
+        if (Files.exists(filePath)) {
+            Files.delete(filePath);
+        }
+    }
 }
