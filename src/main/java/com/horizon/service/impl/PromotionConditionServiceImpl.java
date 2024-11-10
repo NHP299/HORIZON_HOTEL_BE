13c6e8d51132f5fb8082665737252ec0c463734b
dinhhung1084 <dinhhung41335@gmail.com>
2024-11-10 22:57:09 +0700

    add constructor

--- src/main/java/com/horizon/service/impl/PromotionConditionServiceImpl.java
+++ src/main/java/com/horizon/service/impl/PromotionConditionServiceImpl.java
@@ -1,8 +1,16 @@
 package com.horizon.service.impl;
 
+import com.horizon.repository.PromotionConditionRepository;
 import com.horizon.service.PromotionConditionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PromotionConditionServiceImpl implements PromotionConditionService {
+    private final PromotionConditionRepository promotionConditionRepository;
+
+    @Autowired
+    private PromotionConditionServiceImpl(PromotionConditionRepository promotionConditionRepository) {
+        this.promotionConditionRepository = promotionConditionRepository;
+    }
 }
--- src/main/java/com/horizon/service/impl/PromotionTypeServiceImpl.java
+++ src/main/java/com/horizon/service/impl/PromotionTypeServiceImpl.java
@@ -1,8 +1,16 @@
 package com.horizon.service.impl;
 
+import com.horizon.repository.PromotionTypeRepository;
 import com.horizon.service.PromotionTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PromotionTypeServiceImpl implements PromotionTypeService {
+    private final PromotionTypeRepository promotionTypeRepository;
+
+    @Autowired
+    private PromotionTypeServiceImpl(PromotionTypeRepository promotionTypeRepository) {
+        this.promotionTypeRepository = promotionTypeRepository;
+    }
 }
