823df7572aec852b5f410570c67dac32c21c9e18
Hoai2004 <levanhoai2004@gmail.com>
2024-11-11 15:09:34 +0700

    update validation

--- src/main/java/com/horizon/dto/PromotionDto.java
+++ src/main/java/com/horizon/dto/PromotionDto.java
@@ -1,8 +1,11 @@
 package com.horizon.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -11,13 +14,37 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class PromotionDto {
     private Integer id;
+    @NotBlank(message = "Name cannot be blank")
+    @Size(max = 100, message = "Name must be less than 100 characters")
     private String name;
+
     private String description;
+
+    @NotBlank(message = "Discount condition cannot be blank")
     private String discountCondition;
+
+    @NotNull(message = "Discount cannot be null")
     private Double discount;
+
+    @NotNull(message = "Start time cannot be null")
+    @DateTimeFormat(pattern = "HH:mm:ss, dd-MM-yyyy")
+    @JsonFormat(pattern = "HH:mm:ss, dd-MM-yyyy")
     private LocalDateTime startTime;
+
+    @NotNull(message = "End time cannot be null")
+    @DateTimeFormat(pattern = "HH:mm:ss, dd-MM-yyyy")
+    @JsonFormat(pattern = "HH:mm:ss, dd-MM-yyyy")
     private LocalDateTime endTime;
+
+    @NotNull(message = "Maximum usage cannot be null")
+    @Min(value = 1, message = "Maximum usage must be at least 1")
     private Integer maxUsage;
+
+    @NotBlank(message = "Voucher type cannot be blank")
+    @Size(max = 50, message = "Voucher type must be less than 50 characters")
     private String voucherType;
+
+    @NotNull(message = "Maximum amount cannot be null")
+    @Min(value = 0, message = "Maximum amount must be at least 0")
     private Integer maxAmount;
 }
