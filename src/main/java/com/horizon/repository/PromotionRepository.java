b08ebda22a56455fa4591822d986e0209f678017
Hoai2004 <levanhoai2004@gmail.com>
2024-11-14 16:58:52 +0700

    update new method

--- src/main/java/com/horizon/repository/PromotionRepository.java
+++ src/main/java/com/horizon/repository/PromotionRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -23,5 +24,17 @@ public interface PromotionRepository extends JpaRepository<Promotion, Integer> {
     @Query("SELECT p FROM Promotion p WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :promotionName, '%')) AND CURRENT_TIMESTAMP BETWEEN p.startTime AND p.endTime")
     Page<Promotion> findPromotionByNameContainingAndAvailable(@Param("promotionName") String promotionName, Pageable pageable);
 
+    @Query("SELECT p FROM Promotion p " +
+            "JOIN p.promotionCondition pc " +
+            "JOIN pc.promotionType pt " +
+            "WHERE " +
+            "(CURRENT_TIMESTAMP BETWEEN p.startTime AND p.endTime) AND " +
+            "(" +
+            "   (pt.name = 'days_of_booking' AND :daysOfBooking >= pc.value) OR " +
+            "   (pt.name = 'rooms_of_booking' AND :roomsOfBooking >= pc.value) OR " +
+            "   (pt.name = 'booking_time_range' AND CURRENT_TIME BETWEEN pc.startTime AND pc.endTime)" +
+            ")")
+    List<Promotion> findApplicablePromotions(@Param("daysOfBooking") int daysOfBooking,
+                                             @Param("roomsOfBooking") int roomsOfBooking);
 
 }
