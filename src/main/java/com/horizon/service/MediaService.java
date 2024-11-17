25a501982df0c6f988d3a627ac6b2b0ca2551c0c
thongnqt188 <thongnqt188@gmail.com>
2024-11-17 19:17:44 +0700

    update service

--- src/main/java/com/horizon/service/MediaService.java
+++ src/main/java/com/horizon/service/MediaService.java
@@ -2,26 +2,17 @@ package com.horizon.service;
 
 import com.horizon.dto.MediaDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Map;
 
-@Service
 public interface MediaService {
-
     ResponseEntity<Map<String, Object>> createNewMedia(MultipartFile file, Integer roomTypeId);
-
     ResponseEntity<Map<String, Object>> deleteMedia(Integer mediaId);
-
     ResponseEntity<Map<String, Object>> updateMedia(Integer mediaId, MultipartFile file);
-
     List<MediaDto> getMediaByRoomType(Integer roomTypeId);
-
     List<MediaDto> getMediaByRoomName(String roomName);
-
     MediaDto getMediaById(Integer mediaId);
-
     List<MediaDto> getAllMedia();
 }
