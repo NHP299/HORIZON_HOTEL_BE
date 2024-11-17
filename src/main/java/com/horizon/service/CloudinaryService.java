9dfb0d07a697842da44ff2bb5d35b36c6257af4b
thongnqt188 <thongnqt188@gmail.com>
2024-11-17 19:12:35 +0700

    add config for cloudinary

---
+++ src/main/java/com/horizon/service/CloudinaryService.java
@@ -0,0 +1,2 @@
+package com.horizon.service;public class CloudinaryService {
+}
--- src/main/java/com/horizon/service/FileService.java
+++
@@ -1,8 +0,0 @@
-package com.horizon.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileService {
-    String saveFile(MultipartFile file, String uploadDir) throws Exception;
-    void deleteFile(String filename, String uploadDir) throws Exception;
-}
