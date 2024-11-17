3211948ba73fd9bd8bf4bc90aa30260b9cc40d5c
thongnqt188 <thongnqt188@gmail.com>
2024-11-17 19:13:18 +0700

    service for cloudinary

--- src/main/java/com/horizon/service/CloudinaryService.java
+++ src/main/java/com/horizon/service/CloudinaryService.java
@@ -1,2 +1,16 @@
-package com.horizon.service;public class CloudinaryService {
+package com.horizon.service;
+
+import com.horizon.domain.Media;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface CloudinaryService {
+    String uploadImage(MultipartFile file) throws IOException;
+
+    void deleteImage(String publicId) throws IOException;
+
+    String updateImage(MultipartFile file, Media media) throws IOException;
+
+    String getImageUrl(String publicId);
 }
