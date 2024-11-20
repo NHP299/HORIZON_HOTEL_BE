34e3d6c00bb43f3fbb284e7f461860aec292d598
unknown <dinhtcpd08605@fpt.edu.vn>
2024-11-20 21:04:07 +0700

    update service

--- src/main/java/com/horizon/service/RoleService.java
+++ src/main/java/com/horizon/service/RoleService.java
@@ -5,10 +5,13 @@ import com.horizon.dto.RoleDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface RoleService {
     RoleDto createRole(RoleDto roleDto);
     RoleDto updateRole(RoleDto roleDto, Integer id  );
     RoleDto getRoleById(Integer id);
     void deleteRoleById(Integer id);
     Page<RoleDto> getAllRoles(Pageable pageable);
+    List<RoleDto> getAllRoles();
 }
