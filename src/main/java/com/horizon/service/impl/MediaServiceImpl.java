cea6f3e2325614e08dcf205bf8f59fa5e4c2a7e8
thongnqt188 <thongnqt188@gmail.com>
2024-11-20 20:58:37 +0700

    update mediaserviceImpl

--- src/main/java/com/horizon/service/impl/MediaServiceImpl.java
+++ src/main/java/com/horizon/service/impl/MediaServiceImpl.java
@@ -3,22 +3,17 @@ package com.horizon.service.impl;
 import com.horizon.domain.Media;
 import com.horizon.domain.RoomType;
 import com.horizon.dto.MediaDto;
-import com.horizon.exception.ExceptionHandlerService;
 import com.horizon.mapper.MediaMapper;
 import com.horizon.repository.MediaRepository;
-import com.horizon.repository.RoomRepository;
 import com.horizon.repository.RoomTypeRepository;
 import com.horizon.service.CloudinaryService;
 import com.horizon.service.MediaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class MediaServiceImpl implements MediaService {
@@ -30,113 +25,101 @@ public class MediaServiceImpl implements MediaService {
     private RoomTypeRepository roomTypeRepository;
 
     @Autowired
-    private RoomRepository roomRepository;
+    private CloudinaryService cloudinaryService;
 
     @Autowired
     private MediaMapper mediaMapper;
 
-    @Autowired
-    private ExceptionHandlerService exceptionHandlerService;
+    @Override
+    public MediaDto create(MultipartFile file, Integer roomTypeId) {
+        RoomType roomType = getRoomType(roomTypeId);
+        String path = uploadFile(file);
 
-    @Autowired
-    private CloudinaryService cloudinaryService;
+        Media media = new Media();
+        media.setPath(path);
+        media.setRoomType(roomType);
+        media = mediaRepository.save(media);
 
-    private Media getMediaByIdOrThrow(Integer mediaId) {
-        return mediaRepository.findById(mediaId)
-                .orElseThrow(() -> new RuntimeException("Media not found with ID: " + mediaId));
+        return mediaMapper.toDto(media);
     }
 
-    private RoomType getRoomTypeOrThrow(Integer roomTypeId) {
+    @Override
+    public MediaDto update(Long id, MultipartFile file) {
+        Media media = getMedia(id);
+        String oldPublicId = cloudinaryService.getPublicId(media.getPath());
+        validatePublicId(oldPublicId);
+
+        String newPath = uploadFile(file);
+        cloudinaryService.delete(oldPublicId);
+
+        media.setPath(newPath);
+        media = mediaRepository.save(media);
+
+        return mediaMapper.toDto(media);
+    }
+
+    @Override
+    public void delete(Long id) {
+        Media media = getMedia(id);
+        String publicId = cloudinaryService.getPublicId(media.getPath());
+        cloudinaryService.delete(publicId);
+
+        mediaRepository.delete(media);
+    }
+
+    @Override
+    public List<MediaDto> getAll() {
+        return mediaRepository.findAll().stream()
+                .map(mediaMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public MediaDto getById(Long id) {
+        Media media = getMedia(id);
+        return mediaMapper.toDto(media);
+    }
+
+    @Override
+    public List<MediaDto> getByRoomTypeId(Integer roomTypeId) {
+        RoomType roomType = getRoomType(roomTypeId);
+        List<Media> mediaList = mediaRepository.findByRoomType(roomType);
+
+        return mediaList.stream()
+                .map(mediaMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<MediaDto> getByRoomName(String roomName) {
+        RoomType roomType = roomTypeRepository.findByName(roomName)
+                .orElseThrow(() -> new RuntimeException("RoomType with name " + roomName + " not found"));
+
+        List<Media> mediaList = mediaRepository.findByRoomType(roomType);
+
+        return mediaList.stream()
+                .map(mediaMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+
+    private RoomType getRoomType(Integer roomTypeId) {
         return roomTypeRepository.findById(roomTypeId)
-                .orElseThrow(() -> new RuntimeException("Room type not found with ID: " + roomTypeId));
+                .orElseThrow(() -> new RuntimeException("RoomType not found"));
     }
 
-    @Override
-    public ResponseEntity<Map<String, Object>> createNewMedia(MultipartFile file, Integer roomTypeId) {
-        if (file.isEmpty()) {
-            return exceptionHandlerService.handleFileUploadError("File is empty.");
+    private Media getMedia(Long id) {
+        return mediaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Media not found"));
+    }
+
+    private String uploadFile(MultipartFile file) {
+        return cloudinaryService.upload(file);
+    }
+
+    private void validatePublicId(String publicId) {
+        if (publicId == null || publicId.isEmpty()) {
+            throw new RuntimeException("The old image public ID is invalid.");
         }
-
-        try {
-            RoomType roomType = getRoomTypeOrThrow(roomTypeId);
-            String publicId = cloudinaryService.uploadImage(file);
-
-            Media media = new Media();
-            media.setRoomType(roomType);
-            media.setPublicId(publicId);
-            media.setPath(cloudinaryService.getImageUrl(publicId));
-
-            MediaDto mediaDto = mediaMapper.mediaToMediaDto(mediaRepository.save(media));
-            return exceptionHandlerService.createResponse("Media uploaded successfully.", mediaDto, HttpStatus.CREATED);
-        } catch (IOException e) {
-            return exceptionHandlerService.handleFileUploadError("Error uploading image: " + e.getMessage());
-        }
-    }
-
-    @Override
-    public ResponseEntity<Map<String, Object>> updateMedia(Integer mediaId, MultipartFile file) {
-        if (file.isEmpty()) {
-            return exceptionHandlerService.handleFileUploadError("File is empty.");
-        }
-
-        try {
-            Media existingMedia = getMediaByIdOrThrow(mediaId);
-
-            String newPublicId = cloudinaryService.updateImage(file, existingMedia);
-
-            existingMedia.setPublicId(newPublicId);
-            existingMedia.setPath(cloudinaryService.getImageUrl(newPublicId));
-
-            MediaDto updatedMediaDto = mediaMapper.mediaToMediaDto(existingMedia);
-
-            mediaRepository.save(existingMedia);
-
-            return exceptionHandlerService.createResponse("Media updated successfully.", updatedMediaDto, HttpStatus.OK);
-        } catch (Exception e) {
-            return exceptionHandlerService.handleGeneralError("Error updating media: " + e.getMessage());
-        }
-    }
-
-    @Override
-    public ResponseEntity<Map<String, Object>> deleteMedia(Integer mediaId) {
-        try {
-            Media media = getMediaByIdOrThrow(mediaId);
-
-            String publicId = media.getPublicId();
-            if (publicId == null || publicId.isEmpty()) {
-                return exceptionHandlerService.handleGeneralError("Media does not have a valid publicId.");
-            }
-
-            cloudinaryService.deleteImage(publicId);
-            mediaRepository.delete(media);
-
-            return exceptionHandlerService.createResponse("Media deleted successfully.", null, HttpStatus.OK);
-        } catch (Exception e) {
-            return exceptionHandlerService.handleGeneralError("Failed to delete media: " + e.getMessage());
-        }
-    }
-
-    @Override
-    public List<MediaDto> getMediaByRoomType(Integer roomTypeId) {
-        RoomType roomType = getRoomTypeOrThrow(roomTypeId);
-        return mediaMapper.mediaListToMediaDtoList(mediaRepository.findByRoomType(roomType));
-    }
-
-    @Override
-    public List<MediaDto> getMediaByRoomName(String roomName) {
-        RoomType roomType = roomRepository.findByName(roomName)
-                .orElseThrow(() -> new RuntimeException("Room not found with name: " + roomName)).getRoomType();
-        return mediaMapper.mediaListToMediaDtoList(mediaRepository.findByRoomType(roomType));
-    }
-
-    @Override
-    public MediaDto getMediaById(Integer mediaId) {
-        Media media = getMediaByIdOrThrow(mediaId);
-        return mediaMapper.mediaToMediaDto(media);
-    }
-
-    @Override
-    public List<MediaDto> getAllMedia() {
-        return mediaMapper.mediaListToMediaDtoList(mediaRepository.findAll());
     }
 }
