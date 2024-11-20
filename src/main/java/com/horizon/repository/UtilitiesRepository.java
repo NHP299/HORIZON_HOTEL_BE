376256f708428463b821cec6d6a47741c6d85d4a
Hoai2004 <levanhoai2004@gmail.com>
2024-11-20 15:06:09 +0700

    remove pagination

--- src/main/java/com/horizon/repository/UtilitiesRepository.java
+++ src/main/java/com/horizon/repository/UtilitiesRepository.java
@@ -1,28 +1,28 @@
 package com.horizon.repository;
 
 import com.horizon.domain.Utilities;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UtilitiesRepository extends JpaRepository<Utilities, Integer> {
-    Page<Utilities> findByNameContainingIgnoreCase(String name, Pageable pageable);
-    Page<Utilities> findByRoomType_NameContainingIgnoreCase(String roomTypeName, Pageable pageable);
+    List<Utilities> findByNameContainingIgnoreCase(String name);
+    List<Utilities> findByRoomType_NameContainingIgnoreCase(String roomTypeName);
 
     @Query("SELECT u FROM Utilities u " +
             "JOIN u.roomType rt " +
             "JOIN rt.roomList r " +
             "WHERE r.id = :roomId")
-    Page<Utilities> findByRoomId(@Param("roomId") Integer roomId, Pageable pageable);
+    List<Utilities> findByRoomId(@Param("roomId") Integer roomId);
 
     @Query("SELECT u FROM Utilities u " +
             "JOIN u.roomType rt " +
             "JOIN rt.roomList r " +
             "WHERE r.name = :roomName")
-    Page<Utilities> findByRoomName(@Param("roomName") String roomName, Pageable pageable);
+    List<Utilities> findByRoomName(@Param("roomName") String roomName);
 
 }
