f346ab879771f4d0ac36753cd3110f13c226acbc
thongnqt188 <thongnqt188@gmail.com>
2024-11-28 19:29:41 +0700

    update bannertype service

--- src/main/java/com/horizon/service/BannerTypeService.java
+++ src/main/java/com/horizon/service/BannerTypeService.java
@@ -5,9 +5,9 @@ import com.horizon.dto.BannerTypeDto;
 import java.util.List;
 
 public interface BannerTypeService {
-    String createBannerType(BannerTypeDto bannerTypeDto);
-    String updateBannerType(Integer id, BannerTypeDto bannerTypeDto);
-    String deleteBannerType(Integer id);
+    BannerTypeDto createBannerType(BannerTypeDto bannerTypeDto);
+    BannerTypeDto updateBannerType(Integer id, BannerTypeDto bannerTypeDto);
+    void deleteBannerType(Integer id);
     List<BannerTypeDto> getAllBannerTypes();
     BannerTypeDto getBannerTypeById(Integer id);
 }
