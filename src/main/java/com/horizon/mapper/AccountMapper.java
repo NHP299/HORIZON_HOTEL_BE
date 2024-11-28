e66579a635195954f365d3e9960f527ad5893827
thongnqt188 <thongnqt188@gmail.com>
2024-11-28 19:27:31 +0700

    mapper for account

--- src/main/java/com/horizon/mapper/AccountMapper.java
+++ src/main/java/com/horizon/mapper/AccountMapper.java
@@ -1,4 +1,12 @@
 package com.horizon.mapper;
 
+import com.horizon.domain.Account;
+import com.horizon.dto.AccountDto;
+
+import java.util.List;
+
 public interface AccountMapper {
+    Account toEntity(AccountDto accountDto);
+    AccountDto toDto(Account account);
+    List<AccountDto> toDtoList(List<Account> accounts);
 }
