f2416528d37f35e999ba5197140fe4f959679130
thongnqt188 <thongnqt188@gmail.com>
2024-11-28 19:29:00 +0700

    service for account

--- src/main/java/com/horizon/service/AccountService.java
+++ src/main/java/com/horizon/service/AccountService.java
@@ -1,5 +1,14 @@
 package com.horizon.service;
 
+import com.horizon.dto.AccountDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public interface AccountService {
-   
+    AccountDto create(AccountDto accountDto, MultipartFile profilePicture);
+    AccountDto update(Integer id, AccountDto accountDto, MultipartFile profilePicture);
+    void delete(Integer id);
+    List<AccountDto> getAll();
+    AccountDto parseAccountDto(String accountDtoJson);
 }
