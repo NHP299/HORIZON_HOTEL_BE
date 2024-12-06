685171cb545fc7f44404dd825d90b6ec2177899d
dinhhung1084 <dinhhung41335@gmail.com>
2024-12-06 17:34:34 +0700

    integrate vnpay into the project

---
+++ src/main/java/com/horizon/controller/wallet/PaymentController.java
@@ -0,0 +1,36 @@
+package com.horizon.controller.wallet;
+
+import com.horizon.domain.Payment;
+import com.horizon.dto.PaymentDTO;
+import com.horizon.response.ResponseObject;
+import com.horizon.service.PaymentService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("${spring.application.api-prefix}/payment")
+@RequiredArgsConstructor
+public class PaymentController {
+    private final PaymentService paymentService;
+
+    @GetMapping("/vn-pay")
+    public ResponseObject<PaymentDTO.VNPayResponse> pay(HttpServletRequest request) {
+
+        return new ResponseObject<>(HttpStatus.OK, "Success", paymentService.createVnPayPayment(request));
+    }
+
+    @GetMapping("/vn-pay-callback")
+    public ResponseObject<PaymentDTO.VNPayResponse> payCallbackHandler(HttpServletRequest request) {
+        String status = request.getParameter("vnp_ResponseCode");
+        if (status.equals("00")) {
+            //
+            return new ResponseObject<>(HttpStatus.OK, "Success",null);
+        } else {
+            return new ResponseObject<>(HttpStatus.BAD_REQUEST, "Failed", null);
+        }
+    }
+}
