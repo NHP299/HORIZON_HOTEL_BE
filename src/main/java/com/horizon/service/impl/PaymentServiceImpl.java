5093645b7cab999bd58e4ed773ca01b2ab77436c
dinhhung1084 <dinhhung41335@gmail.com>
2024-12-12 19:41:07 +0700

    remove bankCode from HttpServletRequest

--- src/main/java/com/horizon/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/horizon/service/impl/PaymentServiceImpl.java
@@ -35,12 +35,9 @@ public class PaymentServiceImpl implements PaymentService {
     @Override
     public PaymentDTO.VNPayResponse createVnPayPayment(HttpServletRequest request) {
         long amount = Integer.parseInt(request.getParameter("amount")) * 100L;
-        String bankCode = request.getParameter("bankCode");
         Map<String, String> vnpParamsMap = vnPayConfig.getVNPayConfig();
         vnpParamsMap.put("vnp_Amount", String.valueOf(amount));
-        if (bankCode != null && !bankCode.isEmpty()) {
-            vnpParamsMap.put("vnp_BankCode", bankCode);
-        }
+        vnpParamsMap.put("vnp_BankCode", "NCB");
         vnpParamsMap.put("vnp_IpAddr", VNPayUtil.getIpAddress(request));
         String queryUrl = VNPayUtil.getPaymentURL(vnpParamsMap, true);
         String hashData = VNPayUtil.getPaymentURL(vnpParamsMap, false);
