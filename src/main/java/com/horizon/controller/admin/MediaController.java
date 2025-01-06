9cf5582381330f123a049edd0a1bc431297e1feb
dinhhung1084 <dinhhung41335@gmail.com>
2025-01-06 20:34:36 +0700

    update media

--- src/main/java/com/horizon/controller/admin/MediaController.java
+++ src/main/java/com/horizon/controller/admin/MediaController.java
@@ -1,8 +1,12 @@
 package com.horizon.controller.admin;
 
 import com.horizon.dto.MediaDto;
+import com.horizon.response.ResponseObject;
 import com.horizon.service.MediaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,52 +15,52 @@ import java.util.List;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/admin/medias")
+@RequestMapping("${spring.application.api-prefix-admin}/medias")
 public class MediaController {
     @Autowired
     private MediaService mediaService;
 
     @PostMapping("/create")
-    public ResponseEntity<MediaDto> create(@RequestParam("file") MultipartFile file,
+    public ResponseObject<?> create(@RequestParam("file") MultipartFile file,
                                            @RequestParam Integer roomTypeId) {
         MediaDto mediaDto = mediaService.create(file, roomTypeId);
-        return ResponseEntity.ok(mediaDto);
+        return new ResponseObject<>(HttpStatus.OK, "Success", mediaDto);
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<MediaDto> update(@PathVariable Long id,
+    public ResponseObject<?> update(@PathVariable Long id,
                                            @RequestParam("file") MultipartFile file) {
         MediaDto mediaDto = mediaService.update(id, file);
-        return ResponseEntity.ok(mediaDto);
+        return new ResponseObject<>(HttpStatus.OK, "Success", mediaDto);
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
+    public ResponseObject<?> delete(@PathVariable Long id) {
         mediaService.delete(id);
-        return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping
-    public ResponseEntity<List<MediaDto>> getAll() {
-        List<MediaDto> mediaDto = mediaService.getAll();
-        return ResponseEntity.ok(mediaDto);
+        return new ResponseObject<>(HttpStatus.OK, "Success", null);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<MediaDto> getById(@PathVariable Long id) {
+    public ResponseObject<?> getById(@PathVariable Long id) {
         MediaDto mediaDto = mediaService.getById(id);
-        return ResponseEntity.ok(mediaDto);
+        return new ResponseObject<>(HttpStatus.OK, "Success", mediaDto);
+    }
+
+    @GetMapping
+    public ResponseObject<?> getAll(Pageable pageable) {
+        Page<MediaDto> mediaDto = mediaService.getAll(pageable);
+        return new ResponseObject<>(HttpStatus.OK, "Success", mediaDto);
     }
 
     @GetMapping("/roomType/{roomTypeId}")
-    public ResponseEntity<List<MediaDto>> getByRoomTypeId(@PathVariable Integer roomTypeId) {
-        List<MediaDto> mediaDto = mediaService.getByRoomTypeId(roomTypeId);
-        return ResponseEntity.ok(mediaDto);
+    public ResponseObject<?> getByRoomTypeId(@PathVariable Integer roomTypeId, Pageable pageable) {
+        Page<MediaDto> mediaDto = mediaService.getByRoomTypeId(roomTypeId, pageable);
+        return new ResponseObject<>(HttpStatus.OK, "Success", mediaDto);
     }
 
-    @GetMapping("/roomName/{roomName}")
-    public ResponseEntity<List<MediaDto>> getByRoomName(@PathVariable String roomName) {
-        List<MediaDto> mediaDto = mediaService.getByRoomName(roomName);
-        return ResponseEntity.ok(mediaDto);
+    @GetMapping("/roomTypeName")
+    public ResponseObject<?> getByRoomTypeName(@RequestParam String roomTypeName, Pageable pageable) {
+        Page<MediaDto> mediaDto = mediaService.getByRoomTypeName(roomTypeName, pageable);
+        return new ResponseObject<>(HttpStatus.OK, "Success", mediaDto);
     }
 }
--- src/main/java/com/horizon/service/MediaService.java
+++ src/main/java/com/horizon/service/MediaService.java
@@ -2,6 +2,8 @@ package com.horizon.service;
 
 
 import com.horizon.dto.MediaDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -11,8 +13,9 @@ public interface MediaService {
     MediaDto update(Long id, MultipartFile file);
     void delete(Long id);
     MediaDto getById(Long id);
-    List<MediaDto> getByRoomTypeId(Integer roomTypeId);
-    List<MediaDto> getByRoomName(String roomName);
     List<MediaDto> getAll();
+    Page<MediaDto> getAll(Pageable pageable);
+    Page<MediaDto> getByRoomTypeId(Integer roomTypeId, Pageable pageable);
+    Page<MediaDto> getByRoomTypeName(String roomName, Pageable pageable);
 }
 
