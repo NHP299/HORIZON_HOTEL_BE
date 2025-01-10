2776e3b21f8b3e9489ac4cbd8839905da8f68719
unknown <dinhtcpd08605@fpt.edu.vn>
2025-01-10 11:24:17 +0700

    add security config

--- src/main/java/com/horizon/config/SecurityConfig.java
+++ src/main/java/com/horizon/config/SecurityConfig.java
@@ -1,23 +1,56 @@
 package com.horizon.config;
 
+import com.horizon.domain.Account;
+import com.horizon.repository.AccountRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
 @AllArgsConstructor
 public class SecurityConfig {
     private final PasswordEncoder passwordEncoder;
+    private final AccountRepository accountRepository;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll());
-        return http.build();
+    public UserDetailsService userDetailsService() {
+        return email -> accountRepository
+                .findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("Can't find account with email " + email));
     }
 
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config
+    ) throws Exception {
+        return config.getAuthenticationManager();
+    }
 }
+
+
