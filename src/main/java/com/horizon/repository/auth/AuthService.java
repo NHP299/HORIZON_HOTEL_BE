8a778012f5a1875e274da6dd6523848c91ecab50
unknown <dinhtcpd08605@fpt.edu.vn>
2025-01-12 19:49:44 +0700

    delete auth in repo

--- src/main/java/com/horizon/repository/auth/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.horizon.repository.auth;
-
-import java.io.IOException;
-import java.util.Map;
-
-public interface AuthService {
-    String generateAuthUrl(String loginType);
-    Map<String, Object> authenticateAndFetchProfile(String code, String loginType) throws IOException;
-}
